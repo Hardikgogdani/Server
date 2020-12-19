055162acee67e266560386bce543d85963382e37
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-19 12:47:52 +0530

    change

---
+++ src/app/users/model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    age: String,
+    country: String,
+    phone: Number,
+    address: String,
+    gender: String,
+    email: String,
+    password: String,
+    isActive: Boolean
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Users', UsersSchema);
---
+++ src/config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: 'mongodb://localhost:27017/hr',
+    secret: 'supersecret'
+}
